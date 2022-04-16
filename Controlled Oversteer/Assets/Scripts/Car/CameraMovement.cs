702fc3a21aa070b2d5bdf58612c5db54ea2b14e3
SunsetPrune <carterbilawchuk@gmail.com>
2022-04-16 16:56:05 -0400

    Set up Car Physics

---
+++ Controlled Oversteer/Assets/Scripts/Car/CameraMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CameraMovement : MonoBehaviour {
+
+	public Transform player;
+	public Vector3 offset;
+	public Vector3 offsetdirection;
+	public float smoothspeed = 0.125f;
+	public float smoothdirection = 0.125f;
+	
+	
+	void FixedUpdate () 
+	{
+		lookat();
+		Follow();
+	}
+
+
+
+	void lookat()
+	{
+		Vector3 _lookDir = player.position - transform.position;
+		Quaternion rot = Quaternion.LookRotation(_lookDir, Vector3.up);
+		transform.rotation = Quaternion.Lerp(transform.rotation, rot, smoothdirection * Time.deltaTime);
+	}
+
+	void Follow()
+	{
+		Vector3 targetpos = player.position + (player.forward * offset.z + player.right * offset.x + player.up * offset.y);
+		transform.position = Vector3.Lerp(transform.position, targetpos, smoothspeed * Time.deltaTime);
+	}
+
+	public void switchCamera()
+	{
+		offset.z = offset.z * -1;
+	}
+
+
+}
---
+++ Controlled Oversteer/Assets/Scripts/Car/CarController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate () 
+	{
+
+	}
+}
